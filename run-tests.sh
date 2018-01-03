f3b92be04dfa830f6b911fdd07987dab36f5b75b
Donii Sergii <doniysa@gmail.com>
2018-01-03 17:28:34 +0200

    fix routes test

--- run-tests.sh
+++ run-tests.sh
@@ -1,27 +1,27 @@
 #!/usr/bin/env /bin/bash
 
-rm -rf vendor
-rm -rf composer.lock
-composer install
-
-# Run for lumen
-if [ -f "codeception.yml" ]; then
-    rm codeception.yml
-fi
-cp composer.json composer-bkp.json
-cp codeception-lumen.yml codeception.yml
-sleep 1
-rm -rf vendor
-rm -rf composer.lock
-composer require --dev laravel/lumen-framework:5.* --no-interaction
-php tests/replace_autoload.php
-vendor/bin/codecept run --coverage --coverage-xml
-composer remove --dev laravel/lumen-framework --no-interaction
+#rm -rf vendor
+#rm -rf composer.lock
+#composer install
+#
+## Run for lumen
+#if [ -f "codeception.yml" ]; then
+#    rm codeception.yml
+#fi
+#cp composer.json composer-bkp.json
+#cp codeception-lumen.yml codeception.yml
+#sleep 1
+#rm -rf vendor
+#rm -rf composer.lock
+#composer require --dev laravel/lumen-framework:5.* --no-interaction
+#php tests/replace_autoload.php
+#vendor/bin/codecept run --coverage --coverage-xml
 
 # Run for laravel
 if [ -f "codeception.yml" ]; then
     rm codeception.yml
 fi
+cp composer-bkp.json composer.json
 cp codeception-laravel.yml codeception.yml
 sleep 1
 rm -rf vendor
@@ -29,7 +29,7 @@ rm -rf composer.lock
 composer require --dev laravel/framework:5.* --no-interaction
 php tests/replace_autoload.php
 vendor/bin/codecept run --coverage --coverage-xml
-composer remove --dev laravel/framework --no-interaction
+
 if [ -f "codeception.yml" ]; then
     rm codeception.yml
 fi
--- tests/unit/RoutesCallCest.php
+++ tests/unit/RoutesCallCest.php
@@ -2,6 +2,8 @@
 
 require_once __DIR__.'/../_support/CallTest.php';
 
+use Illuminate\Support\Facades\Artisan;
+
 class RoutesCallCest
 {
     /**
@@ -13,31 +15,32 @@ class RoutesCallCest
 
     public function _before(UnitTester $tester)
     {
-//        Artisan::setFacadeApplication(app());
-//        Artisan::call('wamp:run-server', [
-//            '--realm'         => 'realm',
-//            '--host'          => '127.0.0.1',
-//            '--port'          => 9192,
-//            '--in-background' => '--in-background',
-//            '--no-debug'      => '--no-debug',
-//        ]);
-//        sleep(2);
-//        Artisan::call('wamp:register-routes', [
-//            '--realm'         => 'realm',
-//            '--host'          => '127.0.0.1',
-//            '--port'          => 9192,
-//            '--in-background' => '--in-background',
-//            '--no-debug'      => '--no-debug',
-//        ]);
-//        sleep(1);
+        Artisan::setFacadeApplication(app());
+        Artisan::call('wamp:run-server', [
+            '--realm'         => 'realm',
+            '--host'          => '127.0.0.1',
+            '--port'          => 9192,
+            '--in-background' => '--in-background',
+            '--no-debug'      => '--no-debug',
+        ]);
+        sleep(3);
+        Artisan::call('wamp:register-routes', [
+            '--realm'         => 'realm',
+            '--host'          => '127.0.0.1',
+            '--port'          => 9192,
+            '--route-path'    => __DIR__.'/../app/routes.php',
+            '--in-background' => '--in-background',
+            '--no-debug'      => '--no-debug',
+        ]);
+        sleep(3);
 
         $this->tester = $tester;
     }
 
     public function _after(UnitTester $tester)
     {
-//        Artisan::call('wamp:stop');
-//        sleep(1);
+        Artisan::call('wamp:stop');
+        sleep(3);
     }
 
     /**
@@ -89,10 +92,9 @@ class RoutesCallCest
         $result = [];
 
         app()->wampClient->on('open', function (\Thruway\ClientSession $session) use (&$result) {
-            $session->subscribe('wamp.test', function (\Thruway\CallResult $res) use (&$result, $session) {
+            $session->subscribe('wamp.test', function ($res) use (&$result, $session) {
                 $result = [
-                    'obj'  => $res,
-                    'data' => $res->getResultMessage()->getArguments(),
+                    'data' => $res,
                 ];
                 $this->closeLoop();
                 $session->getLoop()->stop();
@@ -101,23 +103,12 @@ class RoutesCallCest
                 $this->closeLoop();
                 $session->getLoop()->stop();
             });
-            $session->publish('wamp.test', ['Test Message'], [], ["acknowledge" => true])->then(
-                function () {
-                    echo PHP_EOL."Publish Acknowledged!".PHP_EOL;
-                },
-                function ($error) {
-                    // publish failed
-                    echo PHP_EOL."Publish Error {$error}".PHP_EOL;
-                    $this->closeLoop();
-                }
-            );
+            $session->publish('wamp.test', ['Test Message'], [], ["exclude_me" => false]);
         });
 
         app()->wampClient->start(true);
-        $this->tester->assertArrayHasKey('obj', $result);
         $this->tester->assertArrayHasKey('data', $result);
-        $this->tester->assertInstanceOf(\Thruway\CallResult::class, $result['obj']);
-        $this->tester->assertEquals('test', $result['data'][0]);
+        $this->tester->assertEquals(['Test Message'], $result['data']);
     }
 
     /**
